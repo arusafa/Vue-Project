41d698c4db006586e75528fc8ddbd2fe382974f1
arusafa <arusafacollege@gmail.com>
2023-06-17 21:55:13 -0400

    Vue-Project has been ready to be deployed

---
+++ front-end/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="nav-bar">
+        <router-link to="/products" class="products-link">
+            <div class="logo-wrap">
+                <img :src="logo" />
+            </div>
+        </router-link>
+        <div class="div nav-buttons-wrap">
+            <button @click="signOut" v-if="user">Sign Out</button>
+            <router-link to="/cart">
+                <button class="cart-button">Shopping Cart</button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { getAuth, signOut } from 'firebase/auth'
+
+import logo from '@/assets/logo-hexagon.svg'
+
+export default {
+    name: 'NavBar',
+    props:['user'],
+    data() {
+        return {
+            logo,
+        }
+    },
+    methods: {
+        async signOut() {
+            const auth = getAuth();
+            await signOut(auth);
+            alert('You have been signed out');
+        }
+    }
+}
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>My Shopping Cart</h1>
+  <!--using v-if statement if theres no item-->
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList @remove-from-cart="removeFromCart($event)" :products="cartItems" />
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    <h3>There are no items in your cart</h3>
+  </div>
+</template>
+
+<script>
+
+import ShoppingCartList from '../components/ShoppingCartList.vue';
+import axios from 'axios';
+
+
+export default {
+  name: 'ShoppingCartPage',
+  props: ['user'],
+  components: {
+    ShoppingCartList,
+  },
+  data() {
+    return {
+      cartItems: [],
+    }
+  },
+  watch: {
+    async user(newUserValue) {
+      if (newUserValue) {
+        const cartResponse = await axios.get(`/api/users/${newUserValue.uid}/cart`);
+        const cartItems = cartResponse.data;
+        this.cartItems = cartItems;
+      }
+    }
+  },
+  methods: {
+    async removeFromCart(productId) {
+      const response = await axios.delete(`/api/users/${this.user.uid}/cart/${productId}`);
+      const updatedCart = response.data;
+      this.cartItems = updatedCart;
+    }
+  },
+  async created() {
+
+    if (this.user) {
+      const response = await axios.get(`/api/users/${this.user.uid}/cart`);
+      const cartItems = response.data;
+      this.cartItems = cartItems;
+    }
+  }
+}
+</script>
